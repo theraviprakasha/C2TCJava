b44256fd0bd531251f3ee716822849eb872fac87
theraviprakasha <work.raviprakasha@gmail.com>
2023-10-09 23:00:12 +0530

    testtrycatch1

--- Java/A14Project/src/com/tnsif/exceptionhandling/testtrycatch1.java
+++ Java/A14Project/src/com/tnsif/exceptionhandling/testtrycatch1.java
@@ -2,22 +2,22 @@ package com.tnsif.exceptionhandling;
 
 public class testtrycatch1
 {
-public static void main(String args[])
-{
-	try
+	public static void main(String args[])
 	{
-	int data=50/0;
-	//without the problem
-	//int data=50/5;
-	}
-	catch(ArithmeticException e)
-	{
-	e.printStackTrace();
-	System.out.println("The denominator must be greater than zero");
-	}
-	finally {
-		System.out.println("Simple finally block");
-	}
-	System.out.println("rest of the code...");
+		try
+		{
+		int data=50/0;
+		//without the problem
+		//int data=50/5;
+		}
+		catch(ArithmeticException e)
+		{
+		e.printStackTrace();
+		System.out.println("The denominator must be greater than zero");
+		}
+		finally {
+			System.out.println("Simple finally block");
+		}
+		System.out.println("rest of the code...");
 	}
 }
