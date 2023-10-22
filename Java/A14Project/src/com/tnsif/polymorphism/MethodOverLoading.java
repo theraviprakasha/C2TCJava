64807aeb83038f57ed7f2ca55a58f56f285ccafe
theraviprakasha <work.raviprakasha@gmail.com>
2023-10-22 23:17:08 +0530

    22oct

--- Java/A14Project/src/com/tnsif/polymorphism/MethodOverLoading.java
+++ Java/A14Project/src/com/tnsif/polymorphism/MethodOverLoading.java
@@ -5,11 +5,9 @@ public class MethodOverLoading {
 	public static int addition(int a, int b) {
 		return a + b;
 	}
-	
 	public static float addition(float a, float b) {
 		return a + b;
 	}
-	
 	public static float addition(int a, float b) {
 		return a + b;
 	}
