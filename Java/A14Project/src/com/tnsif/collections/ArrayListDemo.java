f2ac38fdc4beef944bf536cc6c217beef363e885
theraviprakasha <work.raviprakasha@gmail.com>
2023-10-25 21:31:15 +0530

    25oct

--- Java/A14Project/src/com/tnsif/collections/ArrayListDemo.java
+++ Java/A14Project/src/com/tnsif/collections/ArrayListDemo.java
@@ -6,7 +6,6 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
 
-
 public class ArrayListDemo {
 	public static void main(String[] args) {
 		List list1 = new ArrayList();// creates empty raw ArrayList (allows heterogeneous elements)
